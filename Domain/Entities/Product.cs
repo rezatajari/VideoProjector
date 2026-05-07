32984bcd5edc6788f35129f0697bcd8ecc7f8e99
rezatajari <reza.tajari70@gmail.com>
2026-05-07 13:05:46 +0330

    chore

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities;
+
+public class Product
+{
+    public ProductCode Code{ get;private set; }
+    public ProductName Name{ get;private set; }
+    public ProductType Type{ get; private set; }
+
+    private Product(ProductCode code,ProductName name,ProductType type)
+    {
+        Code=code?? throw new ArgumentNullException(nameof(code));
+        Name=name?? throw new ArgumentNullException( nameof(name));
+        Type = type;
+    }
+
+
+    public static Product Create(
+        ProductCode code,
+        ProductName name,
+        ProductType type)
+    {
+        return new Product(code,name,type);
+    }
+}
