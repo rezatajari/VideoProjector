4b8251186e0e2308c7dfac42f06c4c5499ec2c43
rezatajari <reza.tajari70@gmail.com>
2026-05-22 10:15:19 +0330

    redesign

--- Front/Pages/Account/Login.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using VideoProjectorFront.DTOs.Account;
-
-namespace Front.Pages.Account
-{
-    public partial class Login
-    {
-        private readonly  LoginDto _loginModel = new();
-    }
-}
