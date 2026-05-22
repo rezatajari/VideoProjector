ed38b5b6be235e0bfc6e40e1837a5da2f18c925f
rezatajari <reza.tajari70@gmail.com>
2026-05-22 12:36:45 +0330

    feat: Category

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿```csharp
 namespace Domain.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public string Description { get; set; }
+
+        public string MainImage { get; set; }
+
+        public int CategoryId { get; set; }
+
+
+        // Relationships
+        public virtual Category Category { get; set; }
     }
 }
+```
