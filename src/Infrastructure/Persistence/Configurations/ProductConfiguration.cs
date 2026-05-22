bd58b17194d096c7f86050dcb46dcd3e379c662c
rezatajari <reza.tajari70@gmail.com>
2026-05-22 13:17:01 +0330

    chore

---
+++ src/Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+
+        }
+    }
+}
