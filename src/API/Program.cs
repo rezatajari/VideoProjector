7eb289990e1144e2d9aa81f514da7199df160339
rezatajari <reza.tajari70@gmail.com>
2026-05-22 10:50:10 +0330

    feat: swagger done

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,17 +1,26 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Video Projector API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -21,3 +30,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
