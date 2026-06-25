86a5b5b2c1236ab245d043ae8ab8ff00dc8214c2
rezatajari <reza.tajari70@gmail.com>
2026-06-25 14:07:01 +0330

    refactor(client): refactor order submission form using native Blazor EditForm and DataAnnotations

--- Client/Services/ProductService.cs
+++ Client/Services/ProductService.cs
@@ -1,5 +1,6 @@
 ﻿using Shared.Models;
 using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
 
 namespace Client.Services;
 
@@ -14,5 +15,10 @@ public class ProductService(HttpClient http)
     public async Task<Product?> GetProductByIdAsync(int id)
     {
         return await http.GetFromJsonAsync<Product>($"api/products/{id}");
+    }
+
+    public async Task<HttpResponseMessage> CreateOrderAsync(Order order)
+    {
+        return await http.PostAsJsonAsync("api/orders", order);
     }
 }
