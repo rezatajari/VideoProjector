c8eebbc4f0cb270d6726dd3490b0c6705fc597b3
rezatajari <reza.tajari70@gmail.com>
2026-06-25 13:43:44 +0330

    feat: ProductService

---
+++ Client/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Models;
+using System.Net.Http.Json;
+
+namespace Client.Services;
+
+public class ProductService(HttpClient http)
+{
+    public async Task<List<Product>> GetProductsAsync()
+    {
+        var products = await http.GetFromJsonAsync<List<Product>>("api/products");
+        return products ?? [];
+    }
+
+    public async Task<Product?> GetProductByIdAsync(int id)
+    {
+        return await http.GetFromJsonAsync<Product>($"api/products/{id}");
+    }
+}
