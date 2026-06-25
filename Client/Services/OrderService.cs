7df55a7be6aa17aae69ca48fe23e60fbd273fe84
rezatajari <reza.tajari70@gmail.com>
2026-06-25 15:14:12 +0330

    feat: OrderService

---
+++ Client/Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Models;
+using System.Net.Http.Json;
+
+namespace Client.Services;
+
+public class OrderService(HttpClient http)
+{
+
+    public async Task<List<Order>> GetOrdersAsync()
+    {
+        var orders = await http.GetFromJsonAsync<List<Order>>("api/orders");
+        return orders ?? [];
+    }
+
+    public async Task<bool> UpdateOrderStatusAsync(int id, OrderStatus newStatus)
+    {
+        var response = await http.PutAsJsonAsync($"api/orders/{id}/status", newStatus);
+        return response.IsSuccessStatusCode;
+    }
+}
