8891d902fc6b74f63a4197aadeb0c96473ea4ef2
rezatajari <reza.tajari70@gmail.com>
2026-06-25 15:35:13 +0330

    feat(api): install JWT Bearer package and configure authentication pipeline in Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,30 @@
 using API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
@@ -32,6 +55,8 @@ if (app.Environment.IsDevelopment())
     var context = scope.ServiceProvider.GetRequiredService<VideoProjectorDbContext>();
     context.Database.Migrate();
 }
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.UseSwagger();
