361935e725de9dd5e54ed7024c37491776ae2a4c
rezatajari <reza.tajari70@gmail.com>
2026-06-25 15:38:46 +0330

    feat: TokenService

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,35 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Shared.Models;
+
+namespace API.Services;
+
+public class TokenService(IConfiguration configuration)
+{
+    private readonly IConfiguration _config = configuration;
+
+    public string CreateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.Name, user.FullName)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var tokenOptions = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddDays(7),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+    }
+}
