d853c72319afc89631a652099b60a53d9cb68be2
rezatajari <reza.tajari70@gmail.com>
2026-06-25 08:30:26 +0330

    chore(server): install EF Core packages and configure DbContext in infrastructure

---
+++ API/Data/VideoProjectorDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+
+public class VideoProjectorDbContext : DbContext
+{
+    public VideoProjectorDbContext(DbContextOptions<VideoProjectorDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
