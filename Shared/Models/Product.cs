83db4a30bee140d2341a5ba458a293e924a40d8e
rezatajari <reza.tajari70@gmail.com>
2026-06-25 08:34:06 +0330

    feat(shared): create Product entity inheriting from BaseEntity

---
+++ Shared/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Models;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string Category { get; set; } = string.Empty; // Projector, Screen, Accessory
+
+    // بخش مربوط به فروش (اگر نال باشند یعنی کالا فروشی نیست)
+    public decimal? SalePrice { get; set; }
+    public int QuantityForSale { get; set; }
+
+    // بخش مربوط به اجاره (اگر نال باشند یعنی کالا اجاره‌ای نیست)
+    public decimal? RentalPricePerDay { get; set; }
+    public int QuantityForRental { get; set; }
+}
