b1c6d782545cc386af6f8c66b711455cadbc645b
rezatajari <reza.tajari70@gmail.com>
2026-06-25 08:32:05 +0330

    feat(api): add abstract BaseEntityConfiguration to decouple entity defaults via Fluent API

---
+++ API/Data/Configurations/BaseEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shared.Models;
+
+namespace API.Data.Configurations;
+
+public abstract class BaseEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : BaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.CreatedAt)
+               .HasDefaultValueSql("GETDATE()")
+               .IsRequired();
+
+        builder.Property(e => e.IsDeleted)
+               .HasDefaultValue(false)
+               .IsRequired();
+    }
+}
