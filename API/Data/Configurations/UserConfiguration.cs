8b3034f34c817889ab6dc70ebec8903a3c2eaece
rezatajari <reza.tajari70@gmail.com>
2026-06-25 09:08:53 +0330

    feat(api): implement UserConfiguration with unique email constraint

---
+++ API/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shared.Models;
+
+namespace API.Data.Configurations;
+
+public class UserConfiguration : BaseEntityConfiguration<User>
+{
+    public override void Configure(EntityTypeBuilder<User> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(u => u.FullName)
+               .HasMaxLength(100)
+               .IsRequired();
+
+        builder.Property(u => u.Email)
+               .HasMaxLength(150)
+               .IsRequired();
+
+        builder.HasIndex(u => u.Email)
+               .IsUnique();
+
+        builder.Property(u => u.PasswordHash)
+               .IsRequired();
+
+    }
+}
