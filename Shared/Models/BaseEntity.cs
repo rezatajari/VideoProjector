2db16aa88c25fc2a53d9711e55bb2dcc140e0654
rezatajari <reza.tajari70@gmail.com>
2026-06-25 08:23:34 +0330

    feat(shared): create BaseEntity for core architectural database properties

---
+++ Shared/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Models;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
