7350433c46b59474c6df4aa6122f50b105f5bb55
rezatajari <reza.tajari70@gmail.com>
2026-08-13 13:48:45 +0330

    feat: add Category

---
+++ Domain/Categories/Category.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.Categories;
+
+public class Category:BaseEntity
+{
+    
+}
--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -1,9 +1,14 @@
 ﻿using Domain.Abstractions;
+using Domain.Categories;
 
 namespace Domain.Products;
 
 public sealed class Product:BaseEntity
 {
+    public Guid CategoryId { get; init; }
     public Name Name { get; private init; } =new Name(string.Empty);
     public Description? Description { get; private init; }
+    
+    
+    public ICollection<Category>  Categories { get; private init; } = new HashSet<Category>();
 }
