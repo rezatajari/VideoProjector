e6ea53c16ab5338e66466ff312c9a6ea9cb2f320
rezatajari <reza.tajari70@gmail.com>
2026-08-13 13:40:42 +0330

    feat: add base entity

---
+++ Domain/Abstractions/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Abstractions;
+
+public abstract  class BaseEntity
+{
+    public Guid Id { get;protected set; }
+    public DateTime CreatedAt { get; private set; }
+    public bool IsDeleted { get; protected set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        IsDeleted = false;
+    }
+}
