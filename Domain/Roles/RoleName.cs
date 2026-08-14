d7dc06ce8a5084938ab55075951f61f590fbee86
rezatajari <reza.tajari70@gmail.com>
2026-08-14 07:36:25 +0330

    feat: RoleName

--- Domain/Roles/RoleName.cs
+++ Domain/Roles/RoleName.cs
@@ -1,10 +1,22 @@
-﻿namespace Domain.Roles;
+﻿using Domain.Roles.Exceptions;
+
+namespace Domain.Roles;
 
 public sealed record RoleName
 {
+    private const int MaxLength = 50;
+
+    public string Value { get; }
+
     public RoleName(string value)
     {
-        if  (string.IsNullOrWhiteSpace(value))
+        if (string.IsNullOrWhiteSpace(value))
             throw new InvalidRoleNameException("Role name cannot be empty.");
+
+        var normalizedValue = value.Trim();
+        if (normalizedValue.Length > MaxLength)
+            throw new InvalidRoleNameException("Role name is too long.");
+        
+        Value = normalizedValue;
     }
 }
