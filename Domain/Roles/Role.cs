6a4efea3c4f0b7bbb45fafd8c0b94d86127e503a
rezatajari <reza.tajari70@gmail.com>
2026-08-14 07:25:23 +0330

    feat: role

--- Domain/Roles/Role.cs
+++ Domain/Roles/Role.cs
@@ -1,6 +1,19 @@
-﻿namespace Domain.Roles;
+﻿using Domain.Abstractions;
 
-public class Role
+namespace Domain.Roles;
+
+public class Role:BaseEntity
 {
-    
+    public string Name { get; private set; }
+
+    private Role(string name)
+    {
+        Name = name;
+    }
+
+    public static Role Create(string name)
+    {
+        Role role = new Role(name);
+        return role;
+    }
 }
