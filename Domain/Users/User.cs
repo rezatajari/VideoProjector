301524b340b745b46b84ec05d561903fc833fbde
rezatajari <reza.tajari70@gmail.com>
2026-08-14 07:07:42 +0330

    faet: user create

--- Domain/Users/User.cs
+++ Domain/Users/User.cs
@@ -10,5 +10,30 @@ public sealed class User:BaseEntity
     public Email Eamil { get;private set; }
     public PasswordHash PasswordHash { get; private set; }
     public PhoneNumber PhoneNumber { get; private set; }
+
+
+    private User(
+        FullName fullName, 
+        Email email,
+        PasswordHash passwordHash, 
+        PhoneNumber phoneNumber,
+        Guid roleId)
+    {
+        FullName = fullName;
+        Eamil = email;
+        PasswordHash = passwordHash;
+        PhoneNumber = phoneNumber;
+        RoleId = roleId;
+    }
+
+    public static User Create(
+        FullName fullName,
+        Email email,
+        PasswordHash passwordHash,
+        PhoneNumber phoneNumber,
+        Guid roleId)
+    {
+        User user = new User(fullName, email, passwordHash, phoneNumber,roleId);
+    }
     
 }
---
+++ Domain/Users/ValueObjects/PasswordHash.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Users.ValueObjects;
+
+public class PasswordHash
+{
+    
+}
