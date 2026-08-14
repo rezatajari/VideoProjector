10065644c54092ec8f365d83275df409e4a45516
rezatajari <reza.tajari70@gmail.com>
2026-08-14 06:54:13 +0330

    feat: fullname for user

--- Domain/Users/User.cs
+++ Domain/Users/User.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Abstractions;
+using Domain.Users.ValueObjects;
 
 namespace Domain.Users;
 
---
+++ Domain/Users/ValueObjects/Email.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Emit;
+using Domain.Users.Exceptions;
+
+namespace Domain.Users.ValueObjects;
+
+public class Email
+{
+    private const int MaxLength = 100;
+
+    public string Value { get; }
+    
+    public Email(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+            throw new InvalidEmailException("Email cannot be null or empty.");
+        
+        var normalizedValue = value.Trim();
+        if (normalizedValue.Length > MaxLength)
+            throw new InvalidEmailException("Full name cannot exceed 100 characters.");
+        
+        
+    }
+}
